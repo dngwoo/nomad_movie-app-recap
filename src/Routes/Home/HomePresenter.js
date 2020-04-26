c8e9925fe34f61a4a5e1a1deb688d39ae9c36932
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-04-26 15:24:38 +0900

    Section.js Commit

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,8 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Loader from 'Components/Loader';
+import Section from 'Components/Section';
+
+const Container = styled.div`
+  padding: 20px;
+`;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
-  loading ? <Loader /> : null;
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title='현재 상영작'>
+          {nowPlaying.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title='개봉 예정작'>
+          {upcoming.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title='인기 영화'>
+          {popular.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 
 export default HomePresenter;
